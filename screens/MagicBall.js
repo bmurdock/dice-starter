5f788661a4c2359b5c0e16332b4257b9cc0905ee
bmurdock <brian.j.murdock@gmail.com>
2021-05-28 19:39:26 -0600

    styles

--- screens/MagicBall.js
+++ screens/MagicBall.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import ballIcon from '../assets/8ball.png';
 
 export default function MagicBall() {
     return (
         <View>
+
             <Text>Message: </Text>
-            <Button title="Shake" onPress={function(){}} />
+            <TouchableOpacity onPress={function shake(){
+                console.log('shake me');
+            }}>
+                <Image style={[styles.icon]} source={ballIcon} />
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 100,
+        height: 100,
+        margin: 5,
+    },
+})
